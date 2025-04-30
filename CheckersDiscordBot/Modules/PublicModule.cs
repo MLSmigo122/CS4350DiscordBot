2dd626dec575de87a0d950e2224798f37cd9d924
MLSmigo122 <mlsmigo122@gmail.com>
2025-04-30 00:33:21 -0400

    Bot template with TCP listener

---
+++ CheckersDiscordBot/Modules/PublicModule.cs
@@ -0,0 +1,59 @@
+﻿using CheckersDiscordBot.Services;
+using Discord;
+using Discord.Commands;
+
+namespace CheckersDiscordBot.Modules
+{
+    public class PublicModule : ModuleBase<SocketCommandContext>
+    {
+        //Dependency injecetion
+        public PictureService PictureService { get; set; }
+
+
+        [Command("ping")]
+        [Alias("pong", "hello")]
+        public Task async() => ReplyAsync("pong!");
+
+
+        [Command("cat")]
+        public async Task CatAsync()
+        {
+            //Get a stream containing cat picture
+            var stream = await PictureService.GetCatPictureAsync();
+
+            //Stream must be seeked to the beginning before upload
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "cat.png");
+        }
+
+
+        //Get info on user
+        [Command("userinfo")]
+        public async Task UserInfoAsync(IUser user = null)
+        {
+            user ??= Context.User;
+
+            await ReplyAsync(user.ToString());
+        }
+
+
+        // [Remainder] takes the rest of the command's arguments as one argument, rather than splitting every space
+        [Command("echo")]
+        public Task EchoAsync([Remainder] string text)
+            // Insert a ZWSP before the text to prevent triggering other bots!
+            => ReplyAsync('\u200B' + text);
+
+
+        // 'params' will parse space-separated elements into a list
+        [Command("list")]
+        public Task ListAsync(params string[] objects)
+            => ReplyAsync("You listed: " + string.Join("; ", objects));
+
+
+        // Setting a custom ErrorMessage property will help clarify the precondition error
+        [Command("guild_only")]
+        [RequireContext(ContextType.Guild, ErrorMessage = "Sorry, this command must be ran from within a server, not a DM!")]
+        public Task GuildOnlyCommand()
+            => ReplyAsync("Nothing to see here!");
+    }
+}
---
+++ CheckersDiscordBot/Program.cs
@@ -0,0 +1,60 @@
+﻿using CheckersDiscordBot.Services;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CheckersDiscordBot
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            // You should dispose a service provider created using ASP.NET
+            // when you are finished using it, at the end of your app's lifetime.
+            // If you use another dependency injection framework, you should inspect
+            // its documentation for the best way to do this.
+            await using var services = ConfigureServices();
+            var client = services.GetRequiredService<DiscordSocketClient>();
+            var listener = services.GetRequiredService<MyTCPListener>();
+
+            client.Log += LogAsync;
+            services.GetRequiredService<CommandService>().Log += LogAsync;
+
+            // Tokens should be considered secret data and never hard-coded.
+            // We can read from the environment variable to avoid hard coding.
+            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"));
+            await client.StartAsync();
+
+            // Here we initialize the logic required to register our commands.
+            await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+            listener.runListener();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private static Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+
+            return Task.CompletedTask;
+        }
+
+        private static ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton(new DiscordSocketConfig
+                {
+                    GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+                })
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                .AddSingleton<HttpClient>()
+                .AddSingleton<PictureService>()
+                .AddSingleton<MyTCPListener>()
+                .BuildServiceProvider();
+        }
+    }
+}
